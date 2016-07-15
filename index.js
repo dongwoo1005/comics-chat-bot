2369d7027cd68961ca7ceef7c83900f83d953c13
Dongwoo Son <dw.son@thewhalegames.com>
2016-07-15 18:44:34 +0900

    Initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Process application/json
+app.use(bodyParser.json());
+
+// Index route
+app.get('/', function (req, res) {
+	res.send('Hello world, I am a chat bot!')
+});
+
+// for Facebook verification
+app.get('/webhook', function (req, res) {
+	if (req.query['hub.mode'] === 'subscribe' &&
+			req.query['hub.verify_token'] === 'you_better_verify_me') {
+		console.log("Validating webhook");
+		res.status(200).send(req.query['hub.challenge']);
+	}
+	res.send('Error, wrong token')
+});
+
+// Sign up the server
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+});
