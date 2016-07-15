3948154c109cf13afe5a9b2276e250664894ac01
Dongwoo Son <dw.son@thewhalegames.com>
2016-07-15 19:23:48 +0900

    Updated the bot to speak

--- index.js
+++ index.js
@@ -5,6 +5,89 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
+const token = process.env.FB_PAGE_ACCESS_TOKEN
+
+function receivedMessage(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+
+    console.log("Received message for user %d and page %d at %d with message:",
+        senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+
+    var messageId = message.mid;
+
+    // You may get a text or attachment but not both
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+
+    if (messageText) {
+
+        // If we receive a text message, check to see if it matches any special
+        // keywords and send back the corresponding example. Otherwise, just echo
+        // the text we received.
+        switch (messageText) {
+            case 'image':
+                // sendImageMessage(senderID);
+                break;
+
+            case 'button':
+                // sendButtonMessage(senderID);
+                break;
+
+            case 'generic':
+                // sendGenericMessage(senderID);
+                break;
+
+            case 'receipt':
+                // sendReceiptMessage(senderID);
+                break;
+
+            default:
+                sendTextMessage(senderID, messageText);
+        }
+    } else if (messageAttachments) {
+        sendTextMessage(senderID, "Message with attachment received");
+    }
+}
+
+function sendTextMessage(recipientId, messageText) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: messageText
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: PAGE_ACCESS_TOKEN },
+        method: 'POST',
+        json: messageData
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            console.log("Successfully sent generic message with id %s to recipient %s",
+                messageId, recipientId);
+        } else {
+            console.error("Unable to send message.");
+            console.error(response);
+            console.error(error);
+        }
+    });
+}
+
 app.set('port', (process.env.PORT || 5000));
 
 // Process application/x-www-form-urlencoded
@@ -20,12 +103,35 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook', function (req, res) {
-	if (req.query['hub.mode'] === 'subscribe' &&
-			req.query['hub.verify_token'] === 'you_better_verify_me') {
-		console.log("Validating webhook");
-		res.status(200).send(req.query['hub.challenge']);
+
+	var data = req.body;
+
+	// Make sure this is a page subscription
+	if (data.object == 'page') {
+		// Iterate over each entry
+		// There may be multiple if batched
+		data.entry.forEach(function(pageEntry) {
+			var pageId = pageEntry.id;
+			var timeOfEvent = pageEntry.time;
+			
+			// Iterate over each messaging event
+			pageEntry.messaging.forEach(function(messagingEvent) {
+        if (messagingEvent.optin) {
+          // receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          receivedMessage(messagingEvent);
+        } else if (messagingEvent.delivery) {
+          // receivedDeliveryConfirmation(messagingEvent);
+        } else if (messagingEvent.postback) {
+          // receivedPostback(messagingEvent);
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });	
+		});
+
+		res.sendStatus(200);
 	}
-	res.send('Error, wrong token')
 });
 
 // Sign up the server
