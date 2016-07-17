4d18ffe75eaf07518bf544281627ed2cb4b8b5bf
Dongwoo Son <dw.son@thewhalegames.com>
2016-07-17 18:44:43 +0900

    testing json parsing

--- src/modules/battlecomics.js
+++ src/modules/battlecomics.js
@@ -12,7 +12,7 @@ let getPopularWebtoons = () => {
 				console.log('Error: ', response.body.error);
 			}
 			if (!error && response.statusCode == 200) {
-				console.log(body);
+				console.log('body code: ' + body.code);
 			}
 		}
 	);
