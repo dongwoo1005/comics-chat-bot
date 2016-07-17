ed854f31c5d59ec3e9d4f64586fb07029f40a444
Dongwoo Son <dw.son@thewhalegames.com>
2016-07-17 21:33:03 +0900

    Add 더보기 button

--- src/modules/formatter.js
+++ src/modules/formatter.js
@@ -3,19 +3,37 @@
 let formatWebtoons = webtoons => {
 	let elements = [];
 	let webtoonsUrl = "http://www.battlecomics.co.kr/webtoons/"
-	webtoons.forEach(webtoon => {
+	webtoons.forEach((webtoon, index, array) => {
 		let webtoonUrl = webtoonsUrl + webtoon.id;
-		elements.push({
-			title: webtoon.name,
-			subtitle: webtoon.writer,
-			"item_url": webtoonUrl,
-			"image_url": webtoon.image,
-			"buttons": [{
-				"type": "web_url",
-				"url": webtoonUrl,
-				"title": "바로 보기"
-			}]
-		});
+		if (index !== array.length - 1){
+			elements.push({
+				title: webtoon.name,
+				subtitle: webtoon.writer,
+				"item_url": webtoonUrl,
+				"image_url": webtoon.image,
+				"buttons": [{
+					"type": "web_url",
+					"url": webtoonUrl,
+					"title": "바로 보기"
+				}, {
+					"type": "web_url",
+					"url": webtoonsUrl,
+					"title": "더 보기"
+				}]
+			});
+		} else {
+			elements.push({
+				title: webtoon.name,
+				subtitle: webtoon.writer,
+				"item_url": webtoonUrl,
+				"image_url": webtoon.image,
+				"buttons": [{
+					"type": "web_url",
+					"url": webtoonUrl,
+					"title": "바로 보기"
+				}]
+			});
+		}
 	});
 	return {
 		"attachment": {
