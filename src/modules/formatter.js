103c907d6bbceb43157428653aa0a303d8606e22
Dongwoo Son <dw.son@thewhalegames.com>
2016-07-17 21:16:29 +0900

    Add category to the subtitle

--- src/modules/formatter.js
+++ src/modules/formatter.js
@@ -7,7 +7,7 @@ let formatWebtoons = webtoons => {
 		let webtoonUrl = webtoonsUrl + webtoon.id;
 		elements.push({
 			title: webtoon.name,
-			subtitle: webtoon.writer,
+			subtitle: webtoon.writer + '\n' + webtoon.category,
 			"item_url": webtoonUrl,
 			"image_url": webtoon.image,
 			"buttons": [{
